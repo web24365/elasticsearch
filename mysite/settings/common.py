56e6f6b9bacd9a19712e1b0aa9a6403f4ab8122f
web24365 <web24@hanmail.net>
2019-03-05 18:56:12 +0900

    templates 설정 변경

--- mysite/settings/common.py
+++ mysite/settings/common.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
+        'DIRS': [
+            os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
